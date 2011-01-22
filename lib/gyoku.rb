6e78bd65fce87df9d91c5726caea907d68ca8658
rubiii <me@rubiii.com>
2011-01-22 18:59:25 +0100

    removed Gyoku.to_xml with an Array, because it didn't work anyway

--- lib/gyoku.rb
+++ lib/gyoku.rb
@@ -3,12 +3,8 @@ require "gyoku/hash"
 
 module Gyoku
 
-  def self.xml(object)
-    case object
-      when ::Hash  then Hash.to_xml object
-      when ::Array then Array.to_xml object
-      else              raise ArgumentError, "Expected kind_of Array || Hash"
-    end
+  def self.xml(hash)
+    Hash.to_xml hash
   end
 
 end
--- spec/gyoku_spec.rb
+++ spec/gyoku_spec.rb
@@ -7,11 +7,6 @@ describe Gyoku do
       Gyoku::Hash.expects(:to_xml).with(:id => 1)
       Gyoku.xml :id => 1
     end
-
-    it "should translate a given Array to XML" do
-      Gyoku::Array.expects(:to_xml).with([1, 2, 3])
-      Gyoku.xml [1, 2, 3]
-    end
   end
 
 end
