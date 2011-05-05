4d4b23dcfc1144a76bebe4ba5bceef105ab558e5
rubiii <me@rubiii.com>
2011-05-05 15:14:32 +0200

    fixed unpredictable expectation

--- spec/gyoku_spec.rb
+++ spec/gyoku_spec.rb
@@ -4,8 +4,7 @@ describe Gyoku do
 
   describe ".xml" do
     it "should translate a given Hash to XML" do
-      Gyoku::Hash.expects(:to_xml).with({:id => 1}, :element_form_default => :qualified)
-      Gyoku.xml({ :id => 1 }, :element_form_default => :qualified)
+      Gyoku.xml({ :id => 1 }, :element_form_default => :qualified).should == "<id>1</id>"
     end
   end
 
