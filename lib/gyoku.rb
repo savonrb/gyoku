916b3e838343798587a32c9258559052e615f8b3
Alexey Gaziev <alex.gaziev@gmail.com>
2012-04-18 15:00:59 +0400

    Deep duping hash for not affect original objects

--- lib/gyoku.rb
+++ lib/gyoku.rb
@@ -6,7 +6,7 @@ module Gyoku
 
     # Translates a given +hash+ with +options+ to XML.
     def xml(hash, options = {})
-      Hash.to_xml hash.dup, options
+      Hash.to_xml deep_dup(hash), options
     end
 
     # Yields this object for configuration.
@@ -17,6 +17,27 @@ module Gyoku
     # Sets the formula for converting Symbol keys.
     def convert_symbols_to(formula = nil, &block)
       XMLKey.symbol_converter = formula ? formula : block
+    end
+
+    private
+
+    def deep_dup(object)
+      case object
+      when Array
+        temp = []
+        object.each { |it| temp << deep_dup(it) }
+        temp
+      when Hash
+        temp = {}
+        object.each { |k,v| temp[k] = deep_dup(v) }
+        temp
+      else
+        begin
+          object.clone
+        rescue TypeError
+          object
+        end
+      end
     end
 
   end
