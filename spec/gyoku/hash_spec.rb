965ee969c9a5b417474b86a2497b3552d1009903
Kamil Leczycki <camol88@gmail.com>
2013-03-21 17:28:52 +0100

    Added spec.

--- spec/gyoku/hash_spec.rb
+++ spec/gyoku/hash_spec.rb
@@ -256,6 +256,14 @@ describe Gyoku::Hash do
     end
   end
 
+  it "doesn't modify original hash parameter by deleting its attribute keys" do
+    hash = { person: { name: "Johnny", surname: "Bravo", :"@xsi:type" => "People"} }
+    result = "<person xsi:type=\"People\"><name>Johnny</name><surname>Bravo</surname></person>"
+
+    to_xml(hash).should == result
+    hash.should == {:person=>{:name=>"Johnny", :surname=>"Bravo", :"@xsi:type"=>"People"}}
+  end
+
   def to_xml(hash, options = {})
     Gyoku::Hash.to_xml hash, options
   end
