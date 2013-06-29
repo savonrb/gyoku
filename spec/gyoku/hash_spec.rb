0cfcfa95591df2cb6e8fdda0cbbb3dc6d6b57c21
Vlad Bokov <razum2um@mail.ru>
2013-06-30 02:37:13 +0700

    fix issue #31

--- spec/gyoku/hash_spec.rb
+++ spec/gyoku/hash_spec.rb
@@ -212,6 +212,14 @@ describe Gyoku::Hash do
         "category" => [{:@name => 'one'}, {:@name => 'two'}]
       }
       to_xml(hash).should == '<category name="one"></category><category name="two"></category>'
+
+      # issue #31.
+      hash = {
+        :order! => ['foo', 'bar'],
+        'foo' => { :@foo => 'foo' },
+        'bar' => { :@bar => 'bar', 'baz' => { } },
+      }
+      to_xml(hash).should == '<foo foo="foo"></foo><bar bar="bar"><baz></baz></bar>'
     end
 
     it "recognizes array of attributes with content in each" do
