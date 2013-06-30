9c91cf23bd07517f0e8ef999d5c04ffcef847f4b
rubiii <me@rubiii.com>
2013-06-30 13:49:55 +0200

    added coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,15 @@
-require "bundler"
-Bundler.require :default, :development
+require 'bundler'
+Bundler.setup(:default, :development)
+
+unless RUBY_PLATFORM =~ /java/
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    add_filter 'spec'
+  end
+end
+
+require 'gyoku'
+require 'rspec'
